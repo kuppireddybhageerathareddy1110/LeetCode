3c18e6464f94dc60c344dce1e7613f6ea23867e8
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-05-26 15:22:09 +0530

    Time: 1 ms (99.97%), Space: 42.7 MB (44.83%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,3 +1,27 @@
+// class Solution {
+//     public int reverse(int x) {
+
+//         int rev = 0;
+
+//         while (x != 0) {
+
+//             int digit = x % 10;
+//             x /= 10;
+//             int y=(int)Math.pow(2,31)/10;
+//             // Check overflow using constraints
+//             if (rev > y ||
+//                rev < -y) {
+//                 return 0;
+//             }
+
+//             rev = rev * 10 + digit;
+//         }
+
+//         return rev;
+//     }
+// }
+
+
 class Solution {
     public int reverse(int x) {
 
@@ -5,12 +29,18 @@ class Solution {
 
         while (x != 0) {
 
-            int digit = x % 10;
-            x /= 10;
-            int y=(int)Math.pow(2,31)/10;
-            // Check overflow using constraints
-            if (rev > y ||
-               rev < -y) {
+            int digit = x % 10; // get last digit
+            x = x / 10;         // remove last digit
+
+            // Overflow check
+            if (rev > Integer.MAX_VALUE / 10 || 
+               (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            // Underflow check
+            if (rev < Integer.MIN_VALUE / 10 || 
+               (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
                 return 0;
             }
 
