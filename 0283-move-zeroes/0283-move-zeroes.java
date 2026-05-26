6c7f693d86f386bd7d515bb0aeadee9c36727279
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-05-26 15:09:28 +0530

    Time: 2 ms (91.71%), Space: 48 MB (19.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,31 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int index=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[index]=nums[i];
-                index++;
+    public void moveZeroes(int[] arr) {
+        // int index=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         nums[index]=nums[i];
+        //         index++;
+        //     }
+        // }
+        // while(index<nums.length){
+        //     nums[index]=0;
+        //     index++;
+        // }
+int left = 0; // pointer for placing non-zero elements
+
+        for (int right = 0; right < arr.length; right++) {
+
+            // If current element is non-zero
+            if (arr[right] != 0) {
+
+                // Swap elements
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
             }
         }
-        while(index<nums.length){
-            nums[index]=0;
-            index++;
-        }
-
 
     }
 }
