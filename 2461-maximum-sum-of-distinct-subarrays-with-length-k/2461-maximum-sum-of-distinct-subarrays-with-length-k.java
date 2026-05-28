48886d2eba3723f28f608d2a6c59f0d2afa1a21a
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-05-28 15:07:26 +0530

    Time: 9 ms (98.02%), Space: 84.8 MB (98.04%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,36 +1,28 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        long windowSum=0;
-        long maxSum=0;
-        for(int i=0;i<nums.length;i++){
-            windowSum+=nums[i];
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(i>=k){
-                int remove=nums[i-k];
-                windowSum-=remove;
-                map.put(remove,map.get(remove)-1);
-                if(map.get(remove)==0){
-                    map.remove(remove);
-                }
+       int[] freq=new int[100001];
+       long windowSum=0;
+       long maxSum=0;
+       int distinctCount=0;
+       for(int i=0;i<nums.length;i++){
+        windowSum+=nums[i];
+        freq[nums[i]]++;
+        if(freq[nums[i]]==1){
+            distinctCount++;
+        }
+        if(i>=k){
+            int remove=nums[i-k];
+            windowSum-=remove;
+            freq[remove]--;
+            if(freq[remove]==0){
+                distinctCount--;
             }
-            if(i>=k-1&&map.size()==k){
-                maxSum=Math.max(maxSum,windowSum);
-            }
-
-
-
-
-
-
-
+        }
+        if(i>=k-1&&distinctCount==k){
+            maxSum=Math.max(maxSum,windowSum);
         }
 
-
-      return maxSum;
-
-
-
-
+       }
+       return maxSum;
     }
 }
