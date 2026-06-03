07d53140bd2e9a83030ab6f4dfa774fb7fce3342
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-06-03 16:21:50 +0530

    Time: 7 ms (18.73%), Space: 58.9 MB (19.64%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.ts
@@ -0,0 +1,23 @@
+function maxProduct(nums: number[]): number {
+    let maxProd=nums[0];
+    let minProd=nums[0];
+    let result=nums[0];
+    for(let i=1;i<nums.length;i++){
+        let num=nums[i];
+        if(num<0){
+            [maxProd,minProd]=[minProd,maxProd];
+        }
+
+        maxProd=Math.max(num,maxProd*num);
+        minProd=Math.min(num,minProd*num);
+        result=Math.max(result,maxProd);
+
+
+    }
+
+    return result;
+
+
+
+
+};
