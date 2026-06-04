0cac460fe887fa1f19e781efd25a220a73373f64
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-06-04 16:24:24 +0530

    Time: 6 ms (74.70%) | Memory: 67 MB (27.83%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxSum=0;
+        int minSum=0;
+        int currMax=0;
+        int currMin=0;
+
+        for(int num:nums){
+            currMax=Math.max(num,currMax+num);
+            maxSum=Math.max(maxSum,currMax);
+            currMin=Math.min(num,currMin+num);
+            minSum=Math.min(minSum,currMin);
+        }
+return Math.max(maxSum,Math.abs(minSum));
+
+
+    }
+}
