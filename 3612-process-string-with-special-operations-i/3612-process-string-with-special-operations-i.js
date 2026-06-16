d7c88af05bb0d8d7a31e2dfe1129685fff3c9726
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-06-16 19:32:52 +0530

    Time: 153 ms (11.11%), Space: 83 MB (29.63%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var processStr = function(s) {
+    const result=[];
+    for(const ch of s){
+        if(ch>='a' && ch <='z'){
+            result.push(ch);
+        }
+
+        else if(ch==='*'){
+            if(result.length>0){
+                result.pop();
+            }
+        }
+
+        else if(ch==='#'){
+            result.push(...result);
+        }
+        else if(ch==='%'){
+            result.reverse();
+        }
+    }
+
+    return result.join('');
+    
+
+
+
+
+
+};
