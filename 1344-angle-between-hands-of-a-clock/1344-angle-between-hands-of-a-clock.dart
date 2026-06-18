f7f3d8abc08f2c408abe63fcd111012a32b19b18
KUPPIREDDY BHAGEERATHA REDDY <kuppireddy.bhageeratha@gmail.com>
2026-06-18 12:48:49 +0530

    Time: 0 ms (100%), Space: 148 MB (100%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.dart
@@ -0,0 +1,12 @@
+class Solution {
+  double angleClock(int hour, int minutes) {
+    double minuteAngle=minutes*6.0;
+    double hourAngle=(hour%12)*30.0+minutes*0.5;
+    double diff=(hourAngle-minuteAngle).abs();
+    return diff<360-diff?diff:360-diff;
+
+
+
+
+  }
+}
